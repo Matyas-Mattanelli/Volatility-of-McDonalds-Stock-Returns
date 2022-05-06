46078ea9b6c7bd47a96e8bef54d6d9971964e2f0
Matyas-Mattanelli <91661261+Matyas-Mattanelli@users.noreply.github.com>
2022-05-06 14:13:42 +0200

    Some more analysis. Adding a superb article

--- Code.R
+++ Code.R
@@ -1,19 +1,37 @@
 library(quantmod)
 library(rugarch)
+library(aTSA)
 library(tseries)
 library(forecast)
+library(lmtest)
+
 
 #Downloading the data
-MCD_data <- getSymbols("MCD", auto.assign = FALSE, from = "2010-01-01") #Post-covid period ? 
+MCD_data <- getSymbols("MCD", auto.assign = FALSE, from = "2010-01-01", to = "2022-05-01") #Starting 2010-01-01 as the provided data set but extending to April 2022
 
 #Extracting the adjusted close price
 MCD_adj_close <- MCD_data[, 6]
+
+#Plotting Adjusted Close
+plot(MCD_adj_close)
 
 #Calculating logarithmic returns
 MCD_log_returns <- log(MCD_adj_close) - log(lag(MCD_adj_close))
 sum(is.na(MCD_log_returns)) #Only one NA due to differencing
 MCD_log_returns <- na.omit(MCD_log_returns) #Removing the NA
+rm(MCD_adj_close, MCD_data) #Removing unnecessary data from the environment
+
+#Plotting the returns
 plot(MCD_log_returns)
+addEventLines(xts("Withdrawal", as.Date("2022-03-08")), col = "blue", lwd = 2, pos = 2) #Vertical line in time of withdrawal
+#Zooming in
+plot(MCD_log_returns["2021/"])
+addEventLines(xts("Invasion", as.Date("2022-02-24")), col = "red", lwd = 2, pos = 2) #Vertical line in time of invasion
+addEventLines(xts("Withdrawal", as.Date("2022-03-08")), col = "blue", lwd = 2, pos = 4) #Vertical line in time of withdrawal
+
+#Ploting variance
+plot(MCD_log_returns^2)
+addEventLines(xts("Withdrawal", as.Date("2022-03-08")), col = "blue", lwd = 2, pos = 2) #Vertical line in time of withdrawal
 
 #Stationarity
 adf.test(MCD_log_returns) #Stationary
@@ -31,53 +49,186 @@ for (lag_order in c(4, 8, 12)) {
   print(Box.test(MCD_log_returns, type = "Ljung-Box", lag = lag_order)) #Super significant at all lags
 } 
 
-#AR(1)
-ar1 <- Arima(MCD_log_returns, order = c(1, 0, 0))
-summary(ar1)
-
-#ACF and PACF
-par(mfrow=c(1,2))
-acf(ar1$residuals, xlab = "", ylab = "", main = "ACF") #Significant lag 6, 7, 8...
-pacf(ar1$residuals, xlab = "", ylab = "", main = "PACF") #Significant lag 6, 7, 8...
-
-#AR(2)
-ar2 <- Arima(MCD_log_returns, order = c(2, 0, 0))
-summary(ar2)
-
-#ACF and PACF
-par(mfrow=c(1,2))
-acf(ar2$residuals, xlab = "", ylab = "", main = "ACF") #Significant lag 6, 7, 8...
-pacf(ar2$residuals, xlab = "", ylab = "", main = "PACF") #Significant lag 6, 7, 8...
-
-#Auto.arima
-auto.arima(MCD_log_returns, stationary = T, ic = "aic") #ARMA (4,5)
-
-#ARMA(4,5)
-arma45 <- Arima(MCD_log_returns, order = c(4, 0, 5))
-
-#ACF and PACF
-par(mfrow=c(1,2))
-acf(arma45$residuals, xlab = "", ylab = "", main = "ACF") #Only distant lags significant
-pacf(arma45$residuals, xlab = "", ylab = "", main = "PACF") #Only distant lags significant
-
-#Ljung-Box test
-for (lag_order in c(4, 8, 12)) {
-  print(Box.test(arma45$residuals, type = "Ljung-Box", lag = lag_order)) #Cannot reject null
+#Defining a function that estimates given ARMA, returns p-values of coefficients, AIC, and performs the Ljung-Box test. If needed, also plots ACF and PACF
+arma_complete <- function(ar_oder, ma_order, plot_ACF_PACF = F) {
+  arima_model <- Arima(MCD_log_returns, order = c(ar_oder, 0, ma_order)) #Estimate the model
+  print("P-values:", quote = F)
+  print(arima_model$coef/sqrt(diag(arima_model$var.coef))) #P-values of coefficients
+  print(paste0("AIC: ", arima_model$aic), quote = F)
+  for (lag_order in c(4, 8, 12)) {
+    print(Box.test(arima_model$residuals, type = "Ljung-Box", lag = lag_order)) #Ljung-Box test
+  }
+  if (plot_ACF_PACF == T) {
+    par(mfrow=c(1,2))
+    acf(arima_model$residuals, xlab = "", ylab = "", main = "ACF") #Plot ACF
+    pacf(arima_model$residuals, xlab = "", ylab = "", main = "PACF") #Plot PACF
+  }
 }
 
-#GARCH(1,1)
-garch_spec <- ugarchspec(mean.model = list(armaOrder = c(4, 5)), variance.model = list(garchOrder = c(1, 1)))
-garch_fit <- ugarchfit(garch_spec, MCD_log_returns)
-garch_fit #Everything super significant, Specification tests OK, may want to try TARCH since negative bias is significant
+#AR(1)
+arma_complete(1, 0) #AR(1) significant but many dependencies left
+
+#AR(2)
+arma_complete(2, 0) #AR(2) insignificant, dependencies in 8th and 12th lag
+
+#ARMA(1,1)
+arma_complete(1, 1) #MA(1) insignificant, dependencies in 8th and 12th lag
+
+#ARMA(2,2)
+arma_complete(2, 2) #AR(1) and AR(2) insignificant, both MAs significant, still dependencies left
+
+#ARMA(0,3)
+arma_complete(0, 3) #MA(3) insignificant, dependencies in 8th and 12th lag
+
+#Inspecting the suggestions from auto.arima
+auto.arima(MCD_log_returns, stationary = T, ic = "aic") #ARMA (3,2)
+auto.arima(MCD_log_returns, stationary = T, ic = "bic") #ARMA (1,0)
+
+#ARMA(3,2)
+arma_complete(3, 2, T) #Only AR(3) insignificant, dependecies in 8th and 12th lag
+
+#ARMA(4,3)
+arma_complete(4, 3) #Everything significant but dependencies left
+
+#ARMA(4,4)
+arma_complete(4, 4, T) #Couple of terms insignificant, starting to remove dependencies
+
+#ARMA(5,4)
+arma_complete(5, 4, T) #Satisfactory results but the model is pretty complicated
+
+#ARMA(4,5)
+arma_complete(4, 5, T) #Satisfactory results and higher AIC than previous but still pretty complicated
+
+#Comparing the models statistically
+unrestr <- Arima(MCD_log_returns, order = c(4, 0, 5))
+restr <- Arima(MCD_log_returns, order = c(1, 0, 0))
+lrtest(unrestr, restr) #Reject the null of restricted model being better
+
+#Testing for arch-effects
+arma45 <- arima(MCD_log_returns, order = c(4, 0 ,5))
+arch.test(arma45) #Significant arch effects
+Box.test(arma45$residuals^2, type = "Ljung-Box", lag = 8) #Confirmed
+
+#Defining a function that estimates a specified GARCH model
+garch_complete <- function(ar_order, ma_order, arch_order, garch_order) {
+  garch_spec <- ugarchspec(mean.model = list(armaOrder = c(ar_order, ma_order)), variance.model = list(garchOrder = c(arch_order, garch_order)))
+  garch_fit <- ugarchfit(garch_spec, MCD_log_returns)
+  print(garch_fit) #Print the results
+  return(garch_fit) #Store the results
+}
+
+#GARCH(1,1) with ARMA(4,5)
+garch11arma45 <- garch_complete(4, 5, 1, 1) #AR(1), MA(1), AR(3) insignificant, rest super significant, specification tests ok
+
+#GARCH(1,1) with ARMA(3,2) Trying more parsimonious model
+garch11arma32 <- garch_complete(3, 2, 1, 1) #Solver failed to converge
+
+#GARCH(1,1) with ARMA(3,3) Another shot at a parsimonious model
+garch11arma33 <- garch_complete(3, 3, 1, 1) #Solver failed to converge :(
+
+#GARCH(1,1) with ARMA(1,0) Super parsimonious model
+garch11arma10 <- garch_complete(1, 0, 1, 1) #Specification tests OK :D Negative sign bias significant => may wanna try TARCH
+
+#Inspecting standardized residuals
+#GARCH(1,1) with ARMA(4,5)
+resid_norm_garch11arma45 <- residuals(garch11arma45, standardize = TRUE)
+par(mfrow=c(1,2))
+acf(resid_norm_garch11arma45, xlab = "", ylab = "", main = "ACF") #Almost no dependencies
+pacf(resid_norm_garch11arma45, xlab = "", ylab = "", main = "PACF") #Only one dependency in a very distant lag
+#GARCH(1,1) with ARMA(1,0)
+resid_norm_garch11arma10 <- residuals(garch11arma10, standardize = TRUE)
+acf(resid_norm_garch11arma10, xlab = "", ylab = "", main = "ACF") #Very few dependencies
+pacf(resid_norm_garch11arma10, xlab = "", ylab = "", main = "PACF") #Two dependencies
+
+#Testing normality
+jarque.bera.test(resid_norm_garch11arma45) #Reject null of normality
+jarque.bera.test(resid_norm_garch11arma10) #Reject null of normality :(
 
 #Volatility
 dev.off()
-plot(sigma(garch_fit))
+plot(sigma(garch11arma10))
+#lines(sigma(garch11arma45), col = "red") #The lines are basically the same
+addEventLines(xts("Withdrawal", as.Date("2022-03-08")), col = "red", lwd = 2) #Vertical line in time of withdrawal
 
-#iGARCH(1,1)
-igarch_spec <- ugarchspec(mean.model = list(armaOrder = c(0, 0)), variance.model = list(model = "iGARCH", garchOrder = c(1, 1)))
-igarch_fit <- ugarchfit(igarch_spec, MCD_log_returns)
-igarch_fit
+#Zooming in
+dev.off()
+plot(sigma(garch11arma10)["2021/"])
+addEventLines(xts("Invasion", as.Date("2022-02-24")), col = "red", lwd = 2, pos = 2) #Vertical line in time of invasion
+addEventLines(xts("Withdrawal", as.Date("2022-03-08")), col = "blue", lwd = 2, pos = 4) #Vertical line in time of withdrawal
 
-#Volatility
-plot(sigma(igarch_fit))
+#TARCH(1,1) with ARMA(1,0)
+tarch11arma10_spec <- ugarchspec(mean.model = list(armaOrder = c(1, 0)), variance.model = list(model = "gjrGARCH", garchOrder = c(1, 1)))
+tarch11arma10 <- ugarchfit(spec = tarch11arma10_spec, data = MCD_log_returns)
+tarch11arma10
+
+#Checking TARCH residuals
+resid_norm_tarch11arma10 <- residuals(tarch11arma10, standardize = TRUE)
+par(mfrow=c(1,2))
+acf(resid_norm_tarch11arma10, xlab = "", ylab = "", main = "ACF") #Almost no dependencies
+pacf(resid_norm_tarch11arma10, xlab = "", ylab = "", main = "PACF") #2 dependencies at distant lags
+Box.test(resid_norm_tarch11arma10, type = "Ljung-Box", lag = 20) #Cannot reject the null on no autocorrelation
+jarque.bera.test(resid_norm_tarch11arma10) #Reject the null of normality
+
+#TARCH(1,1) with ARMA(1,0)
+tarch11arma45_spec <- ugarchspec(mean.model = list(armaOrder = c(4, 5)), variance.model = list(model = "gjrGARCH", garchOrder = c(1, 1)))
+tarch11arma45 <- ugarchfit(spec = tarch11arma45_spec, data = MCD_log_returns)
+tarch11arma45 #Everything super significant, specification tests ok
+
+#Checking TARCH residuals
+resid_norm_tarch11arma45 <- residuals(tarch11arma45, standardize = TRUE)
+par(mfrow=c(1,2))
+acf(resid_norm_tarch11arma45, xlab = "", ylab = "", main = "ACF") #Almost no dependencies
+pacf(resid_norm_tarch11arma45, xlab = "", ylab = "", main = "PACF") #1 dependency at a distant lag
+Box.test(resid_norm_tarch11arma45, type = "Ljung-Box", lag = 20) #Cannot reject the null on no autocorrelation
+jarque.bera.test(resid_norm_tarch11arma45) #Reject the null of normality
+
+#May wanna consider using the Student's distribution given the shape of the log-returns and the repeatadly rejected hypothesis of residuals' normality
+
+#GARCH(1, 1) with ARMA(1,0) and Student's distribution
+garch11arma10stud_spec <- ugarchspec(mean.model = list(armaOrder = c(1, 0)), variance.model = list(garchOrder = c(1, 1)), distribution.model = "std")
+garch11arma10stud <- ugarchfit(garch11arma10stud_spec, MCD_log_returns)
+garch11arma10stud #Everything super significant, specification tests ok, negative sign bias significant => TARCH
+
+#Inspecting the residuals
+resid_norm_garch11arma10stud <- residuals(garch11arma10stud, standardize = TRUE)
+par(mfrow=c(1,2))
+acf(resid_norm_garch11arma10stud, xlab = "", ylab = "", main = "ACF") #Almost no dependencies
+pacf(resid_norm_garch11arma10stud, xlab = "", ylab = "", main = "PACF") #1 or 2 dependencies at distant lags
+Box.test(resid_norm_garch11arma10stud, type = "Ljung-Box", lag = 20) #Cannot reject the null on no autocorrelation
+jarque.bera.test(resid_norm_garch11arma10stud) #Reject the null of normality
+
+
+#TARCH(1, 1) with ARMA(1,0) and Student's distribution
+tarch11arma10stud_spec <- ugarchspec(mean.model = list(armaOrder = c(1, 0)), variance.model = list(model = "gjrGARCH", garchOrder = c(1, 1)), distribution.model = "std")
+tarch11arma10stud <- ugarchfit(tarch11arma10stud_spec, MCD_log_returns)
+tarch11arma10stud #Everything super significant, specification tests ok
+
+#Inspecting the residuals
+resid_norm_tarch11arma10stud <- residuals(tarch11arma10stud, standardize = TRUE)
+par(mfrow=c(1,2))
+acf(resid_norm_tarch11arma10stud, xlab = "", ylab = "", main = "ACF") #Almost no dependencies
+pacf(resid_norm_tarch11arma10stud, xlab = "", ylab = "", main = "PACF") #1 or 2 dependencies at distant lags
+Box.test(resid_norm_tarch11arma10stud, type = "Ljung-Box", lag = 20) #Cannot reject the null on no autocorrelation
+jarque.bera.test(resid_norm_tarch11arma10stud) #Reject the null of normality
+
+#Plotting volatility
+dev.off()
+plot(sigma(tarch11arma10stud))
+lines(sigma(garch11arma10), col = "red") #The lines are basically the same
+
+#Checking whether alpha + beta is smaller than one (stability condition) - we may want to test that but I don't know how yet
+tarch11arma10stud@fit$coef[4]+tarch11arma10stud@fit$coef[5]
+
+#Adding dummy for the withdrawal
+withdrawal_dummy <- matrix(ifelse(index(MCD_log_returns) >= as.Date("2022-03-08"), 1, 0), ncol = 1)
+tarch11arma10stud_dummy_spec <- ugarchspec(mean.model = list(armaOrder = c(1, 0)), variance.model = list(model = "gjrGARCH", garchOrder = c(1, 1), external.regressors = withdrawal_dummy), distribution.model = "std")
+tarch11arma10stud_dummy <- ugarchfit(tarch11arma10stud_dummy_spec, MCD_log_returns)
+tarch11arma10stud_dummy #External regressor not significant :/
+
+#Trying the same for covid
+covid_dummy <- matrix(ifelse(index(MCD_log_returns) >= as.Date("2020-01-20"), 1, 0), ncol = 1) #First Covid-19 case in USA
+tarch11arma10stud_covid_spec <- ugarchspec(mean.model = list(armaOrder = c(1, 0)), variance.model = list(model = "gjrGARCH", garchOrder = c(1, 1), external.regressors = covid_dummy), distribution.model = "std")
+tarch11arma10stud_covid <- ugarchfit(tarch11arma10stud_covid_spec, MCD_log_returns)
+tarch11arma10stud_covid #Covid super significant => Let's change the topic? :D
+
+
